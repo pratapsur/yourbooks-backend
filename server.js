80b801442e89f6e60a15685757e6fdd936c58c63
LaunchCutgit config --global user.name Pratap <launchcut.dev@gmail.com>
2026-02-20 23:53:48 +0530

    Secure CORS policy

--- server.js
+++ server.js
@@ -8,7 +8,10 @@ const cors = require('cors');
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+    origin: process.env.FRONTEND_URL || '*', // Fallback to * if env is missing
+    credentials: true
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
