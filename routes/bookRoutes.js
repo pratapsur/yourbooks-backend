ed1e70b176e4f5c1753a51dc684c7b72e498a7f7
LaunchCutgit config --global user.name Pratap <launchcut.dev@gmail.com>
2026-02-21 11:18:58 +0530

    Fix Mongoose deprecation warning

--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -117,7 +117,7 @@ router.put('/:id/page', auth, async (req, res) => {
         const book = await Book.findOneAndUpdate(
             { _id: req.params.id, user: req.user.id },
             { currentPage: req.body.currentPage },
-            { new: true }
+            { returnDocument: 'after' } // <--- THE CLEAN MODERN WAY
         );
         if (!book) return res.status(404).json({ error: 'Book not found' });
         res.status(200).json(book);
